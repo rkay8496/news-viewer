bc739b1d33302c6a4421c1344e9ca138a2a29fb8
RyoungGu Kwon <rkay8496@gmail.com>
2021-07-01 20:01:49 +0900

    complete

---
+++ src/components/NewsList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from '../../node_modules/axios/index';
+import usePromise from '../lib/usePromise';
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0b72ad312c40402799e97a4c9b8f7bcc`,
+    );
+  }, [category]);
+
+  // 대기 중일 때
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  // 아직 reponse 값이 설정되지 않았을 때
+  if (!response) {
+    return null;
+  }
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article}></NewsItem>
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
